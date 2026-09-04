82ed3d0497f50436a16264810bda0632bcf66b1b
Immux <immunesicness051@gmail.com>
2026-09-04 23:52:26 +0000

    Create Yumi.py

---
+++ Yumi.py
@@ -0,0 +1,7 @@
+cat << 'EOF' > Yumi.py
+#!/usr/bin/env python3
+import sys
+if __name__ == "__main__":
+    print("🐍 [Yumi Loader] Launcher system initialized in boostrap mode.")
+EOF
+chmod +x Yumi.py
